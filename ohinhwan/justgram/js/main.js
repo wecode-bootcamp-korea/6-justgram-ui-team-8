d5f123974f44dff7275c667e5400517ae79a86aa
Roy Oh <yunoroy@gmail.com>
2022-08-04 23:40:01 +0900

    미션4 완료

--- ohinhwan/justgram/js/main.js
+++ ohinhwan/justgram/js/main.js
@@ -1,12 +1,34 @@
+const replyArr = document.getElementsByClassName("reply");
 const commentArr = document.getElementsByClassName("commentInput");
 const buttonArr = document.getElementsByClassName("commentButton");
+const replyPlace = document.getElementsByClassName("reply-post");
 //const loginById = document.querySelector("loginId");
 
 //임시로 0번 인덱스(1번피드 댓글부터 구현);
-let commentInput;
-let replyPlace;
 document.addEventListener("keyup", validate);
-document.addEventListener("click", postComment);
+
+Array.from(replyArr).forEach((button, index) => {
+  button.addEventListener("click", () => {
+    if (buttonArr[index].disabled === false) {
+      const content = commentArr[index].value;
+      const div = document.createElement("div");
+      const writerId = document.createElement("span"); //화면에 나타야할 순서대로 작성해준다.
+      const writerComment = document.createElement("span");
+      const button = document.createElement("button");
+      writerId.innerText = "commenter";
+      writerId.classList.add("bold");
+      writerComment.innerText = content;
+      button.innerText = "x"; // 삭제 기능 구현
+      replyPlace[index].appendChild(div);
+      replyPlace[index].appendChild(writerId);
+      replyPlace[index].appendChild(writerComment);
+      replyPlace[index].appendChild(button);
+      commentInput = ""; //내용 초기화
+      commentArr[index].value = ""; // 인풋상자 내용 초기화
+    }
+  });
+});
+//document.addEventListener("click", postComment);
 
 function validate() {
   for (let i = 0; i < commentArr.length; i++) {
@@ -17,24 +39,6 @@ function validate() {
       buttonArr[i].disabled = false;
       buttonArr[i].style.cursor = "pointer";
       buttonArr[i].classList.add("buttonActivate");
-      commentInput = commentArr[i].value;
-      replyPlace = document.getElementsByClassName("reply-post")[i];
     }
   }
-}
-
-function postComment() {
-  const br = document.createElement("br");
-  const writerId = document.createElement("span"); //화면에 나타야할 순서대로 작성해준다.
-  const writerComment = document.createElement("span");
-  const button = document.createElement("button");
-  writerId.innerText = "commenter";
-  writerId.classList.add("bold");
-  writerComment.innerText = commentInput;
-  button.innerText = "x";
-  replyPlace.appendChild(br);
-  replyPlace.appendChild(writerId);
-  replyPlace.appendChild(writerComment);
-  replyPlace.appendChild(button);
-  commentInput = "";
 }
