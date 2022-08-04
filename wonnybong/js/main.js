2b7e54eeacafe77f4a40dd2a29fbc533e864b646
Bong <won950705@naver.com>
2022-08-04 17:59:52 +0900

    Add : Mission 5 완료 - feeds.json 파일 생성 - fetch 후 데이터로 html 내용 동적으로 변경

--- wonnybong/js/main.js
+++ wonnybong/js/main.js
@@ -1,6 +1,28 @@
-const commentForms = [...document.getElementsByClassName("comment-form")];
-const user = "2021Bong";
-const commentTimeArr = [...document.getElementsByClassName("post-act-time")];
+const commentForms = [...document.getElementsByClassName("feed-com-form")];
+const commentTimeArr = [...document.getElementsByClassName("feed-cont-time")];
+const idUidArr = [...document.getElementsByClassName("feed-id-uid")];
+const contUidArr = [...document.getElementsByClassName("feed-cont-uid")];
+const feedImgArr = [...document.getElementsByClassName("img")];
+const likeCountArr = [...document.getElementsByClassName("like-count")];
+const commentContentArr = [...document.getElementsByClassName("feed-cont-com")];
+const allCommentArr = [...document.getElementsByClassName("feed-all-comment")];
+let datalist;
+const writer = "guest1";
+fetch("data/feeds.json")
+  .then((res) => res.json())
+  .then((data) => {
+    datalist = data;
+    data.forEach((comment) => {
+      let i = data.indexOf(comment);
+      idUidArr[i].innerText = comment.userName;
+      contUidArr[i].innerText = comment.userName;
+      feedImgArr[i].src = comment.imageSrc;
+      likeCountArr[i].innerText = `좋아요 ${comment.likeCount}개`;
+      commentContentArr[i].innerText = comment.content;
+      allCommentArr[i].innerText = `댓글 ${comment.allComment}개 모두 보기`;
+      commentTimeArr[i].innerText = `${comment.createdTime}시간 전`;
+    });
+  });
 
 const getComment = (e) => {
   let inputText = e.target.value;
@@ -18,11 +40,11 @@ const postComment = (e) => {
   let index = commentForms.indexOf(e.target.form);
   e.preventDefault();
   e.target.form[1].classList.remove("abled");
-  writeComment(e.target.form[0].value, user, index);
+  writeComment(e.target.form[0].value, index);
   e.target.form[0].value = "";
 };
 
-const writeComment = (content, writer, i) => {
+const writeComment = (content, i) => {
   const newComment = document.createElement("p");
   newComment.innerText = content;
   const userId = document.createElement("span");
@@ -30,7 +52,6 @@ const writeComment = (content, writer, i) => {
   userId.classList.add("bold");
   userId.classList.add("mr5");
   newComment.prepend(userId);
-  // console.log(newComment);
   const commentTime = commentTimeArr[i];
   const commentContainer = commentTime.parentNode;
   commentContainer.insertBefore(newComment, commentTime);
