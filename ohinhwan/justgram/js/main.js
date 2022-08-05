2cfed7e7821184de83ee1a9c84941e3c4d6183ac
Roy Oh <yunoroy@gmail.com>
2022-08-05 12:47:05 +0900

    미션 5 완료 리팩토링 진행중

--- ohinhwan/justgram/js/main.js
+++ ohinhwan/justgram/js/main.js
@@ -4,9 +4,32 @@ const buttonArr = document.getElementsByClassName("commentButton");
 const replyPlace = document.getElementsByClassName("reply-post");
 //const loginById = document.querySelector("loginId");
 
+//JSON fetch mission5
+fetch("./data/comments.json")
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res.comments);
+    let arr = res.comments;
+    console.log(arr[0].userName);
+    arr.forEach((element, index) => {
+      const div = document.createElement("div");
+      const writerId = document.createElement("span"); //화면에 나타야할 순서대로 작성해준다.
+      const writerComment = document.createElement("span");
+      const button = document.createElement("button");
+      writerId.innerText = element.userName;
+      writerId.classList.add("bold");
+      writerComment.innerText = element.content;
+      button.innerText = "x"; // 삭제 기능 구현
+      replyPlace[index].appendChild(div);
+      replyPlace[index].appendChild(writerId);
+      replyPlace[index].appendChild(writerComment);
+      replyPlace[index].appendChild(button);
+    });
+  });
+
 //임시로 0번 인덱스(1번피드 댓글부터 구현);
 document.addEventListener("keyup", validate);
-
+//댓글 달기 구현
 Array.from(replyArr).forEach((button, index) => {
   button.addEventListener("click", () => {
     if (buttonArr[index].disabled === false) {
