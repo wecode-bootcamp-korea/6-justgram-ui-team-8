3afa24104844c3c9ad4dbc236dfdae05f6da4820
Bong <won950705@naver.com>
2022-08-05 12:07:15 +0900

    Add : Mission 7 댓글 좋아요, 삭제 기능 추가 - 댓글 생성시 하트 추가 및 좋아요 기능 구현 - 댓글 생성시 삭제 아이콘 추가 및 삭제 기능 구현 - 댓글 삭제 전 comfirm 단계 구현

--- wonnybong/js/main.js
+++ wonnybong/js/main.js
@@ -1,14 +1,14 @@
-const commentForms = [...document.getElementsByClassName("feed-com-form")];
-const commentTimeArr = [...document.getElementsByClassName("feed-cont-time")];
-const idUidArr = [...document.getElementsByClassName("feed-id-uid")];
-const contUidArr = [...document.getElementsByClassName("feed-cont-uid")];
-const feedImgArr = [...document.getElementsByClassName("img")];
-const likeCountArr = [...document.getElementsByClassName("like-count")];
-const commentContentArr = [...document.getElementsByClassName("feed-cont-com")];
-const allCommentArr = [...document.getElementsByClassName("feed-all-comment")];
+const commentForms = [...document.getElementsByClassName('feed-com-form')];
+const commentTimeArr = [...document.getElementsByClassName('feed-cont-time')];
+const idUidArr = [...document.getElementsByClassName('feed-id-uid')];
+const contUidArr = [...document.getElementsByClassName('feed-cont-uid')];
+const feedImgArr = [...document.getElementsByClassName('img')];
+const likeCountArr = [...document.getElementsByClassName('like-count')];
+const commentContentArr = [...document.getElementsByClassName('feed-cont-com')];
+const allCommentArr = [...document.getElementsByClassName('feed-all-comment')];
 let datalist;
-const writer = "guest1";
-fetch("data/feeds.json")
+const writer = 'guest1';
+fetch('data/feeds.json')
   .then((res) => res.json())
   .then((data) => {
     datalist = data;
@@ -29,35 +29,74 @@ const getComment = (e) => {
   let btn = e.target.form[1];
   if (inputText) {
     btn.disabled = false;
-    btn.classList.add("abled");
+    btn.classList.add('abled');
   } else {
     btn.disabled = true;
-    btn.classList.remove("abled");
+    btn.classList.remove('abled');
   }
 };
 
 const postComment = (e) => {
   let index = commentForms.indexOf(e.target.form);
   e.preventDefault();
-  e.target.form[1].classList.remove("abled");
+  e.target.form[1].classList.remove('abled');
   writeComment(e.target.form[0].value, index);
-  e.target.form[0].value = "";
+  e.target.form[0].value = '';
 };
 
 const writeComment = (content, i) => {
-  const newComment = document.createElement("p");
+  const newComment = document.createElement('p');
   newComment.innerText = content;
-  const userId = document.createElement("span");
+  newComment.classList.add('new-com');
+  const userId = document.createElement('span');
   userId.innerText = writer;
-  userId.classList.add("bold");
-  userId.classList.add("mr5");
+  userId.classList.add('bold');
+  userId.classList.add('mr5');
   newComment.prepend(userId);
+  const heart = document.createElement('span');
+  heart.classList.add('icon-setting');
+  heart.classList.add('heart');
+  heart.classList.add('hearts');
+  newComment.append(heart);
+  const xIcon = document.createElement('span');
+  xIcon.classList.add('icon-setting');
+  xIcon.classList.add('x-icon');
+  newComment.append(xIcon);
+  const newComContainer = document.createElement('div');
+  newComContainer.classList.add('new-com-container');
+  newComContainer.append(newComment);
   const commentTime = commentTimeArr[i];
   const commentContainer = commentTime.parentNode;
-  commentContainer.insertBefore(newComment, commentTime);
+  commentContainer.insertBefore(newComContainer, commentTime);
+
+  const commentHeart = [...document.getElementsByClassName('hearts')];
+  const commentX = [...document.getElementsByClassName('x-icon')];
+  commentHeart.forEach((heart) => {
+    heart.addEventListener('click', heartChange);
+  });
+  commentX.forEach((xIcon) => {
+    xIcon.addEventListener('click', removeComment);
+  });
+};
+
+const heartChange = (e) => {
+  let heart = e.target;
+  if (heart.classList.contains('heart')) {
+    heart.classList.remove('heart');
+    heart.classList.add('like-heart');
+  } else {
+    heart.classList.remove('like-heart');
+    heart.classList.add('heart');
+  }
+};
+
+const removeComment = (e) => {
+  if (confirm('정말 삭제하시겠습니까?')) {
+    e.path[1].remove();
+  }
 };
 
 commentForms.forEach((form) => {
-  form.addEventListener("input", getComment);
-  form[1].addEventListener("click", postComment);
+  form.addEventListener('input', getComment);
+  form[1].addEventListener('click', postComment);
 });
