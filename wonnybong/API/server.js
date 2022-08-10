a2c6b883ba55b626d15c826407cff108bdc118c9
Bong <won950705@gmail.com>
2022-08-11 00:08:52 +0900

    Add : prisma 추가 - prisma package 설치 - prisma와 DB 연결 - prisma와 서버 연결 - 디렉토리 정리

--- wonnybong/API/server.js
+++ wonnybong/API/server.js
@@ -1,20 +1,29 @@
 const http = require('http');
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
 const { sendPostings } = require('./sendPostings');
 const { feeds } = require('./feeds');
 const { profiles } = require('./profiles');
 
 const app = express();
 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'hi!' });
 });
 app.get('/posts', sendPostings);
 app.get('/feeds', feeds);
 app.get('/profiles', profiles);
+app.get('/comments', async (req, res) => {
+  const createComment =
+    await prisma.$queryRaW`INSERT INTO comments(id, content) VALUES (${id},${content})`;
+});
 
 const server = http.createServer(app);
 
 server.listen(8000, (req, res) => {
-  console.log({ message: 'server is running PORT 8000' });
+  console.log({ message: 'Listening to requests on port 8000' });
 });
